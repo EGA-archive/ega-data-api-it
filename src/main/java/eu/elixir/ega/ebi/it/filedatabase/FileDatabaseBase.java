fb23aa0bd927a71b7dce8a9e5df7efb7effe8c60
amohan <anandmohan777@gmail.com>
2019-02-13 13:51:16 +0000

    changing validation for filedatabase

--- src/main/java/eu/elixir/ega/ebi/it/filedatabase/FileDatabaseBase.java
+++ src/main/java/eu/elixir/ega/ebi/it/filedatabase/FileDatabaseBase.java
@@ -1,7 +1,5 @@
 package eu.elixir.ega.ebi.it.filedatabase;
 
-import java.util.InputMismatchException;
-
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.specification.RequestSpecification;
@@ -15,21 +13,24 @@ public class FileDatabaseBase {
     String indexId;
 
     public FileDatabaseBase() {
-        try {
-            RestAssured.baseURI = System.getProperty("file.url");
 
-            if (RestAssured.baseURI == null) {
-                throw new InputMismatchException("fileservice host url is null. Pls check configuration in pom.xml.");
-            }
-
-            RestAssured.port = Integer.parseInt(System.getProperty("file.port"));
-            datasetId = System.getProperty("datasetId");
-            fileId = System.getProperty("fileId");
-            indexId = System.getProperty("indexId");
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        if (System.getProperty("file.url") == null) {
+            throw new IllegalArgumentException("fileservice host url is null. Pls check configuration in pom.xml.");
+        } else if (System.getProperty("indexId") == null) {
+            throw new IllegalArgumentException("IndexId value is null. Pls check configuration in pom.xml.");
+        } else if (System.getProperty("datasetId") == null) {
+            throw new IllegalArgumentException("DatasetId value is null. Pls check configuration in pom.xml.");
+        } else if (System.getProperty("fileId") == null) {
+            throw new IllegalArgumentException("FileId value is null. Pls check configuration in pom.xml.");
+        } else if (System.getProperty("file.port") == null) {
+            throw new IllegalArgumentException("Port value is null. Pls check configuration in pom.xml.");
         }
+        
+        RestAssured.baseURI = System.getProperty("file.url");
+        RestAssured.port = Integer.parseInt(System.getProperty("file.port"));
+        datasetId = System.getProperty("datasetId");
+        fileId = System.getProperty("fileId");
+        indexId = System.getProperty("indexId");
 
         REQUEST = RestAssured.given().contentType(ContentType.JSON);
     }
