3d0740c74fbda7887d303554bd50a9b49b88c62e
amohan <anandmohan777@gmail.com>
2019-02-13 13:51:36 +0000

    changing validation for key

--- src/main/java/eu/elixir/ega/ebi/it/key/KeyBase.java
+++ src/main/java/eu/elixir/ega/ebi/it/key/KeyBase.java
@@ -1,7 +1,5 @@
 package eu.elixir.ega.ebi.it.key;
 
-import java.util.InputMismatchException;
-
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.specification.RequestSpecification;
@@ -12,20 +10,21 @@ public class KeyBase {
     String keyId;
 
     public KeyBase() {
-        try {
-            RestAssured.baseURI = System.getProperty("key.url");
 
-            if (RestAssured.baseURI == null) {
-                throw new InputMismatchException("Key service host url is null. Pls check configuration in pom.xml.");
-            }
-
-            RestAssured.port = Integer.parseInt(System.getProperty("key.port"));
-            fileId = System.getProperty("fileId");
-            keyId = System.getProperty("keyId");
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        if (System.getProperty("key.url") == null) {
+            throw new IllegalArgumentException("Key service host url is null. Pls check configuration in pom.xml.");
+        } else if (System.getProperty("fileId") == null) {
+            throw new IllegalArgumentException("FileId value is null. Pls check configuration in pom.xml.");
+        } else if (System.getProperty("keyId") == null) {
+            throw new IllegalArgumentException("KeyId value is null. Pls check configuration in pom.xml.");
+        } else if (System.getProperty("key.port") == null) {
+            throw new IllegalArgumentException("Port value is null. Pls check configuration in pom.xml.");
         }
+
+        RestAssured.baseURI = System.getProperty("key.url");
+        RestAssured.port = Integer.parseInt(System.getProperty("key.port"));
+        fileId = System.getProperty("fileId");
+        keyId = System.getProperty("keyId");
 
         REQUEST = RestAssured.given().contentType(ContentType.JSON);
     }
